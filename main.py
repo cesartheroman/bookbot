85baeeb7f11e87323449b7f902ca42f2b5111234
Cesar Roman <cesardroman18@gmail.com>
2024-02-19 16:07:59 -0600

    finished bookbot

---
+++ main.py
@@ -0,0 +1,64 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    char_freq = count_characters(text)
+    generate_report(num_words, char_freq, book_path)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def count_characters(text):
+    char_dict = {}
+    words = text.split()
+
+    for word in words:
+        lower_cased_word = word.lower()
+        for char in lower_cased_word:
+            if char.isalpha():
+                if char not in char_dict:
+                    char_dict[char] = 0
+                char_dict[char] += 1
+
+    return char_dict
+
+
+def sort_on(char_map):
+    return char_map["freq"]
+
+
+def sort_dict(char_map):
+    dict_list = []
+
+    for char in char_map:
+        dict_list.append({"name": char, "freq": char_map[char]})
+
+    dict_list.sort(reverse=True, key=sort_on)
+
+    return dict_list
+
+
+def generate_report(num_words, char_freq, book_path):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    sorted_dict = sort_dict(char_freq)
+
+    for dictionary in sorted_dict:
+        print(
+            f"The {dictionary['name']} character was found {dictionary['freq']} times"
+        )
+
+    print("--- End report ---")
+
+
+main()
